0128a7f1ec7733362c53df39779f7a9b59bcf894
GitJuski <juuso.vainikka@outlook.com>
2025-02-12 16:55:06 +0200

    Add dynamic table functionality and basic styling to admin page

--- renderer/adminRenderer.js
+++ renderer/adminRenderer.js
@@ -1,6 +1,38 @@
+// Function for dynamically creating a table of foods in the administration page
+function showData(data) {
+  const table = document.getElementById("table"); // Get the table element from admin.html. This whole table can be done dynamically. For fast prototyping, I decided to make the table headers static.
+
+  // Goes through all the items in the data and creates the row and cells for them
+  data.forEach(food => {
+    const row = document.createElement("tr");
+    const id = document.createElement("td");
+    const title = document.createElement("td");
+    const desc = document.createElement("td");
+    const rating = document.createElement("td");
+    const category = document.createElement("td");
+
+    // Add the correct data to each cell
+    id.innerText=food.ID;
+    title.innerText=food.Title;
+    desc.innerText=food.Description;
+    rating.innerText=food.Rating;
+    category.innerText=food.Category;
+
+    // Add the elements to table
+    row.appendChild(id);
+    row.appendChild(title);
+    row.appendChild(desc);
+    row.appendChild(rating);
+    row.appendChild(category);
+    table.appendChild(row);
+  });
+
+  // Add the table to the container element
+  document.getElementById("container").appendChild(table);
+  console.log(table);
+}
+
 window.electronAPI.onContentUpdate((data) => {
   console.log("Fetched data:", data);
-});
-
-const button = document.getElementById("refresh").addEventListener("click", () => {
+  showData(data); // calls the showData function and passes the data as a parameter
 });
