f8456660acd0e8cc13662176b57df0893871dcc4
GitJuski <juuso.vainikka@outlook.com>
2025-02-26 20:00:36 +0200

    Add copyright notice

--- database/database.js
+++ database/database.js
@@ -1,3 +1,20 @@
+/*
+    WeeklyFoodlist Desktop Version. Generate random foods to eat for a week.
+    Copyright (C) 2025  Juuso Vainikka
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 
--- preload.js
+++ preload.js
@@ -1,10 +1,11 @@
+// https://www.electronjs.org/docs/latest/tutorial/ipc
 const { contextBridge, ipcRenderer } = require("electron");
 
 // Expose API for main process to renderer process data flow and other way around
 contextBridge.exposeInMainWorld("electronAPI", {
   onContentUpdate: (callback) => ipcRenderer.on("update-content", (_event, data) => callback(data)), // Main to Renderer
   createOperations: (data) => ipcRenderer.send("createOperations", data), // Renderer to Main
-  deleteOperations: (id) => ipcRenderer.send("deleteOperations", id),
-  updateOperations: data => ipcRenderer.send("updateOperations", data)
+  deleteOperations: (id) => ipcRenderer.send("deleteOperations", id), // Renderer to Main
+  updateOperations: data => ipcRenderer.send("updateOperations", data) // Renderer to Main
 });
 
